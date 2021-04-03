fa0ccef1c15672e100f6febec680eef9fd40a372
Vasyl Don <54324315+Vasyl-Don@users.noreply.github.com>
2021-04-04 01:16:40 +0300

    Create Program.cs

---
+++ oop_4/Program.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace oop_4_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] line_1 = new char[] { 'I', ' ', 'l', 'o', 'v', 'e', ' ', 'C', '#' };
+
+            Line S1 = new Line();
+            Line S2 = new Line(line_1);
+            Line S3 = new Line(S1);
+
+            Console.Write("S1 = ");
+            Console.WriteLine(S1.GetLine());
+            Console.Write("S2 = ");
+            Console.WriteLine(S2.GetLine());
+            Console.Write("S3 = ");
+            Console.WriteLine(S3.GetLine());
+
+            S2 = S2 - '#';
+            Console.Write("\nS2 = ");
+            Console.WriteLine(S2.GetLine());
+
+            S1 = S2 + S3;
+            Console.Write("\nS1 = ");
+            Console.WriteLine(S1.GetLine());
+        }
+    }
+}
