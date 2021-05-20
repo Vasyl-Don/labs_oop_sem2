3a10bf6b0b3a356b43edfa918adfddd999323fca
Vasyl Don <54324315+Vasyl-Don@users.noreply.github.com>
2021-05-20 12:08:01 +0300

    Create source.h

---
+++ oop_1/cpp/source.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int length(int);	//визначення довжини числа у двійковій формі
+void isless(int, int, bool&);	//операція порівняння <
+int sum(int, int);	//операція додавання +
